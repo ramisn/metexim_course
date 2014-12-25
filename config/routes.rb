a7e6e19b3f95159fa9fde72565f6bdd49f6ef933
Ramesh K <rameshkror@gmail.com>
2014-12-25 13:11:44 +0530

    Customized Views

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,9 @@
 Rails.application.routes.draw do
-  resources :chapters
-
-  resources :syllabuses
-
-  resources :courses
-
-  get 'user/index'
-
-  get 'home/index'
-
-  devise_for :users, :controllers => {:registrations => "registrations" } 
- 
   root 'home#index'
+  resources :chapters
+  resources :syllabuses
+  resources :courses
+  get 'user/index'
+  get 'home/index'
+  devise_for :users, :controllers => {:registrations => "registrations" } 
 end
